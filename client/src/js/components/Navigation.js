16468e89a96a3af42ff02c1f483db1dc51a5eea2
Dennis S. Hennen <dennis@28studios.com>
2016-01-02 00:25:05 -0500

    Cleanup navigation flickers.

--- client/src/js/components/Navigation.js
+++ client/src/js/components/Navigation.js
@@ -9,13 +9,21 @@ export default class Navigation extends Component {
     const menuClass = menuOpen ? "menus menus-open" : "menus menus-closed";
     const handleOpen = () => onToggleMenu(true);
     const handleClose = () => onToggleMenu(false);
+
+    const navList = [
+      <Link key="1" className="nav-site-name nav-link" to="/">meetingtimer.io</Link>,
+      <Link key="2" className="nav-link" to="/about">about</Link>,
+      <a key="3" className="nav-link" target="_blank" href="https://github.com/dsh/meetingtimer">github</a>
+    ];
+    // I had to dupe the menu list because I was getting weird CSS transitions when re-sizing the browser window.
     return (
       <div className={menuClass}>
         <a href="#" onClick={handleOpen} className="mobile-menu-button"></a>
-        <div onClick={handleClose} className="nav-link-list">
-          <Link className="nav-site-name nav-link" to="/">meetingtimer.io</Link>
-          <Link className="nav-link" to="/about">about</Link>
-          <a className="nav-link" target="_blank" href="https://github.com/dsh/meetingtimer">github</a>
+        <div className="nav-link-list">
+          {navList}
+        </div>
+        <div onClick={handleClose} className="mobile-nav-link-list">
+          {navList}
         </div>
         <div className="screen-mask" onClick={handleClose}></div>
       </div>
